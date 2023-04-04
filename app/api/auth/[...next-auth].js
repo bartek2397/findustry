dc5fd1c60a9495edbbbcce3f297cf22384bd28bb
bartek2397 <bartosz.wszolek97@gmail.com>
2023-04-04 20:31:38 +0200

    registration api completed

---
+++ app/api/auth/[...next-auth].js
@@ -0,0 +1,64 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../prisma/client'
+import Credentials from 'next-auth/providers/credentials'
+import { compare } from 'bcryptjs'
+
+const adapter = PrismaAdapter(prisma)
+
+export const authOptions = {
+    adapter: adapter,
+    providers: [
+        Credentials({
+            id: 'credentials',
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password'
+                },
+            },
+            async authorize(credentials) {
+                if(!credentials?.email || !credentials?.password) {
+                    throw new Error('Email and password required')
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user || !user.hashedPassword) {
+                    throw new Error('Email does not exist')
+                }
+
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword)
+
+                if (!isCorrectPassword) {
+                    throw new Error('Incorrect Password')
+                }
+
+                return user;
+            }
+
+        })
+    ],
+    pages: {
+        signIn: '/login',
+    },
+    debug: process.env.NODE_ENV === 'development', 
+    session: {
+        strategy: 'jwt',
+    },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
+
+export default NextAuth(authOptions)
